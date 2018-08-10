1f484eec02f52473221f400e4a25504a17f06783
Tácio Nery <tacionery@gmail.com>
2018-08-10 16:07:07 -0300

    I was born

---
+++ create_cluster_with_kops.sh
@@ -0,0 +1,41 @@
+set -e
+
+NAME=$1
+ZONES=$2
+NODE_SIZE=$3
+MASTER_SIZE=$4
+
+if [ -z "$NAME" ]; then
+	echo "Cluster Name cannot be empty!!!"
+	return 0
+fi
+
+if [ -z "$ZONES" ]; then
+	echo "Zones cannot be empty!!!"
+	return 0
+fi
+
+if [ -z "$NODE_SIZE" ]; then
+	echo "Nodes Size cannot be empty!!!"
+	return 0
+fi
+
+if [ -z "$MASTER_SIZE" ]; then
+	echo "Master Size cannot be empty!!!"
+	return 0
+fi
+
+export NAME=$NAME
+export KOPS_STATE_STORE="s3://$NAME"
+
+echo "- Creating state store.."
+aws s3 mb $KOPS_STATE_STORE
+
+echo "- Creating k8s cluster with kops.."
+kops create cluster --zones $ZONES --name $NAME --dns private --dns-zone $HOSTZONEID --vpc $VPCID --topology private --networking kopeio-vxlan --node-size $NODE_SIZE --master-size $MASTER_SIZE
+
+echo "- Building k8s cluster with kops.."
+kops update cluster $NAME --yes
+
+echo "Name: $NAME"
+echo "KopsStateStore: $KOPS_STATE_STORE"
---
+++ create_vpc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+VPC=$1
+REGION=$2
+
+if [ -z "$VPC" ]; then
+	echo "VPC Name cannot be empty!!!"
+	return 0
+fi
+
+if [ -z "$REGION" ]; then
+	echo "REGION cannot be empty!!!"
+	return 0
+fi
+
+echo "---- Creating VPC ----"
+VPCID=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 | jq -r '.Vpc.VpcId')
+aws ec2 create-tags --resources $VPCID --tags Key=Name,Value=$VPC
+aws ec2 modify-vpc-attribute --vpc-id $VPCID --enable-dns-support "{\"Value\":true}"
+aws ec2 modify-vpc-attribute --vpc-id $VPCID --enable-dns-hostnames "{\"Value\":true}"
+
+echo "---- Creating Internet Gateway ----"
+IGW=$(aws ec2 create-internet-gateway | jq -r '.InternetGateway.InternetGatewayId')
+
+echo "---- Attaching Internet Gateway to VPC ----"
+aws ec2 attach-internet-gateway --vpc-id $VPCID --internet-gateway-id $IGW
+
+echo "---- Creating Hosted Zone ----"
+ID=$(uuidgen)
+export HOSTZONEID=$(aws route53 create-hosted-zone --name $VPC --vpc "VPCRegion=$REGION,VPCId=$VPCID" --caller-reference $ID --hosted-zone-config PrivateZone=true --hosted-zone-config Comment="$VPC" | jq -r '.HostedZone.Id' | cut -d '/' -f 3)
+
+export VPCID=$VPCID
+export IGW=$IGW
+export HOSTZONEID=$HOSTZONEID
+
+echo "---- Summary ----"
+echo "VPC Id: $VPCID"
+echo "VPC Name: $VPC"
+echo "Internet Gateway: $IGW"
+echo "Hosted Zone: $HOSTZONEID"
