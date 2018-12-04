3a7cd129ac95a70686f176fa76e965a28fd57788
Tácio Nery <tacionery@gmail.com>
2018-12-04 15:53:06 -0300

    Update create_vpc.sh

--- create_vpc.sh
+++ create_vpc.sh
@@ -4,6 +4,8 @@ set -e
 
 VPC=$1
 REGION=$2
+ZONE_A="a"
+ZONE_B="b"
 
 if [ -z "$VPC" ]; then
 	echo "VPC Name cannot be empty!!!"
@@ -30,6 +32,10 @@ aws ec2 attach-internet-gateway --vpc-id $VPCID --internet-gateway-id $IGW
 echo "> Creating Hosted Zone"
 ID=$(uuidgen)
 export HOSTZONEID=$(aws route53 create-hosted-zone --name $VPC --vpc "VPCRegion=$REGION,VPCId=$VPCID" --caller-reference $ID --hosted-zone-config PrivateZone=true --hosted-zone-config Comment="$VPC" | jq -r '.HostedZone.Id' | cut -d '/' -f 3)
+
+echo "> Creating Subnets"
+aws ec2 create-subnet --vpc-id $VPCID --cidr-block 10.0.100.0/24 --availability-zone $REGION$ZONE_A
+aws ec2 create-subnet --vpc-id $VPCID --cidr-block 10.0.101.0/24 --availability-zone $REGION$ZONE_B
 
 export VPCID=$VPCID
 export IGW=$IGW
